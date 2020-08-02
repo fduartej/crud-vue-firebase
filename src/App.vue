d9198ed15d8145e676de1bdc859febdd47058a0b
Fred Duarte <gestion_atlassian@inteligogroup.com>
2020-08-01 20:30:57 -0500

    Initial Repository

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,133 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <el-popover
+      placement="bottom"
+      title="New Employee"
+      width="200"
+      trigger="click">
+      <el-input
+        placeholder="Tom Cruise"
+        v-model="name"
+        @blur="createEmployee(name, date)"></el-input>
+      <el-button round slot="reference" type="success" >Add New Employee</el-button>
+    </el-popover>
+    <el-table
+      :data="
+        employeesData.filter(
+          (data) =>
+            !search || data.name.toLowerCase().includes(search.toLowerCase())
+        )
+      "
+      style="width: 100%;"
+    >
+      <el-table-column label="Date" prop="date"> </el-table-column>
+      <el-table-column label="Name" prop="name"> </el-table-column>
+      <el-table-column align="right">
+        <template slot="header" :slot-scope="scope">
+          <el-input v-model="search" size="mini" placeholder="Type to search" />
+        </template>
+        <template slot-scope="scope">
+          <el-popover
+            placement="bottom"
+            title="Edit Employee"
+            width="200"
+            trigger="click"
+          >
+            <el-input
+              placeholder="John Doe"
+              v-model="scope.row.name"
+              @blur="updateEmployee(scope.row.id, scope.row.name, date)"
+            ></el-input>
+            <el-button size="mini" slot="reference">Edit</el-button>
+          </el-popover>
+          <el-button
+            size="mini"
+            type="danger"
+            @click="deleteEmployee(scope.row.id)"
+            >Delete</el-button
+          >
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import firebase from "./firebaseInit";
+const db = firebase.firestore();
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      name: "",
+      date: new Date().toISOString().slice(0, 10),
+      employeesData: [],
+      search: "",
+    };
+  },
+  methods: {
+    createEmployee(name, date) {
+      if (name != "") {
+        db.collection("employees")
+          .add({ date: date, name: name })
+          .then(() => {
+            console.log("Document successfully written!");
+            this.readEmployees();
+          })
+          .catch((error) => {
+            console.error("Error writing document: ", error);
+          });
+        this.name = "";
+      }
+    },
+    readEmployees() {
+      this.employeesData = [];
+      db.collection("employees")
+        .get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            this.employeesData.push({
+              id: doc.id,
+              name: doc.data().name,
+              date: doc.data().date,
+            });
+            console.log(doc.id, " => ", doc.data());
+          });
+        })
+        .catch((error) => {
+          console.log("Error getting documents: ", error);
+        });
+    },
+    updateEmployee(id, name, date) {
+      db.collection("employees")
+        .doc(id)
+        .update({
+          name: name,
+          date: date,
+        })
+        .then(() => {
+          console.log("Document successfully updated!");
+          this.readEmployees();
+        })
+        .catch((error) => {
+          // The document probably doesn't exist.
+          console.error("Error updating document: ", error);
+        });
+    },
+    deleteEmployee(id) {
+      db.collection("employees")
+        .doc(id)
+        .delete()
+        .then(() => {
+          console.log("Document successfully deleted!");
+          this.readEmployees();
+        })
+        .catch((error) => {
+          console.error("Error removing document: ", error);
+        });
+    },
+  },
+  mounted() {
+    this.readEmployees();
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
