709642ee0fddfb01a457ddcc398251c90d70968a
Fred <fduartej@users.noreply.github.com>
2020-08-11 15:38:17 -0500

    Update firebaseInit.js

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -2,13 +2,13 @@ import firebase from "firebase";
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyASbp9MaarvLj6v0utUJPQoG86zuZZJChE",
-  authDomain: "webapp-ee6e3.firebaseapp.com",
-  databaseURL: "https://webapp-ee6e3.firebaseio.com",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: ",
   projectId: "webapp-ee6e3",
   storageBucket: "webapp-ee6e3.appspot.com",
   messagingSenderId: "173051186312",
-  appId: "1:173051186312:web:440f870dcabadc015be33d"
+  appId: ""
 };
 
 // Initialize Firebase
